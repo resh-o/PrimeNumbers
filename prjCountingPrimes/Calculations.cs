7448d693e41606c241757903432f8b6ad999abe7
ReshaanST <ST10433877@vcconnect.edu.za>
2025-03-24 04:13:53 +0200

    Add project files.

---
+++ prjCountingPrimes/Calculations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjCountingPrimes
+{
+    class Calculations
+    {
+        public static void SieveOfEratosthenes(int n)
+        {
+            bool[] prime = new bool[n + 1];
+            for (int i = 0; i <= n; i++)
+                prime[i] = true;
+
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p])
+                {
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+
+            int primeCount = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i])
+                    primeCount++;
+            }
+
+            Console.WriteLine($"Prime numbers up to {n}");
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i])
+                    Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n***************************");    
+            Console.WriteLine($"Amount of prime numbers: {primeCount}");
+            Console.WriteLine("\n***************************");
+        }
+    }
+}
---
+++ prjCountingPrimes/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace prjCountingPrimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bStop = false;
+
+            while (!bStop)
+            {
+                Console.Write("\n*************************");
+                Console.Write("\nEnter a number: ");
+                String strInput = Console.ReadLine();
+
+                if (strInput.Equals("="))
+                {
+                    bStop = true;
+                    break;
+                }
+
+                if (int.TryParse(strInput, out int n))
+                {
+                    Calculations.SieveOfEratosthenes(n);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+        }
+    }
+}
